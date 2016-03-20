548ef93503a42a817e40de289e265039fff0368b
MichaelDM <michaelmachiah@gmail.com>
2016-03-20 13:11:34 -0400

    all styling done, making sure now that choosing input source returns value and that whole API-sentiment analysis works

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-$('button').on('click',function(){
-    // storing the api to seach for in object and storing value of button in appropriate object
-    ajaxObject.apiToSearch = $('#sourceInput option:selected').val();
+$(document).ready(function(){
+  $('button').on('click',function(){
+      // storing the api to seach for in object and storing value of button in appropriate object
+      ajaxObject.apiToSearch = $('#sourceInput option:selected').val();
 
-    // once I transition, $('.cd-dropdown span span').text()
-    // also change the cases in switch statement in apiObjects
+      // once I transition, $('.cd-dropdown span span').text()
+      // also change the cases in switch statement in apiObjects
 
 
-    // making API call according to selected API value
-    var endpoint = allApiObject.selectApiToCall(ajaxObject.apiToSearch);
+      // making API call according to selected API value
+      var endpoint = allApiObject.selectApiToCall(ajaxObject.apiToSearch);
+      console.log('click');
+
+  });
+
+
+  // toggle arrow menu
+  $('.menu-icon-wrapper').on('click', function(){
+    event.preventDefault();
+    $('.menu-icon-wrapper span').toggleClass('menu-icon-wrapper-close');
+      $('.side-menu').toggleClass('side-menu-close');
+  });
+
+  //hiding search when choosing a source
+  $('.cd-dropdown').on('click',function(){
     console.log('click');
-
-});
-
-
-// toggle arrow menu
-$('.menu-icon-wrapper').on('click', function(){
-  event.preventDefault();
-  $('.menu-icon-wrapper span').toggleClass('menu-icon-wrapper-close');
-
-    $('.side-menu').toggleClass('side-menu-close');
-
+    $('#searchButton').toggleClass('cd-dropdown-hide');
+  });
 });
