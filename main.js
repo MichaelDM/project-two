b42210a5291f0f209a0a4987b5f5dbc1a03eab7e
MichaelDM <michaelmachiah@gmail.com>
2016-03-20 10:50:19 -0400

    before adding side menu

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 $('button').on('click',function(){
     // storing the api to seach for in object and storing value of button in appropriate object
     ajaxObject.apiToSearch = $('#sourceInput option:selected').val();
+
+    // once I transition, $('.cd-dropdown span span').text()
+    // also change the cases in switch statement in apiObjects
+
+
     // making API call according to selected API value
     var endpoint = allApiObject.selectApiToCall(ajaxObject.apiToSearch);
     console.log('click');
